19b8346a3d4662ba934e82eaa69c5a652a03bfff
Jaeha Lee <wogkdkrm112@gmail.com>
2023-01-17 23:03:53 +0900

    ✒️Feat: todo list 수정기능 추가
    1. 수정 버튼을 클릭하면 '수정' -> '저장'.
    2. 저장 버튼일 때 todo input에 focus, 수정 가능모드
    3. 수정 후 저장 버튼을 클릭하면 input blur, 저장 완료

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,28 @@ import '../scss/style.scss';
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
+  // todo list 수정
+  $('.main__todo').addEventListener('click', (e) => {
+    // 1. 수정 버튼을 클릭하면 -> 저장 글씨 바뀜, input.focus()
+    // 2. todo-title이 바뀔 수 있게
+    // 3. 다시 저장 버튼을 클릭하면 -> 수정 글씨 바뀌고 입력된 title 저장, input.blur()
+    const editBtn = e.target.closest('li').querySelector('.edit-btn');
+    const editInput = e.target.closest('li').querySelector('.todo-title');
+
+    editBtn.addEventListener('click', () => {
+      if (editBtn.textContent === '수정') {
+        editInput.removeAttribute('readonly');
+        editInput.focus();
+        editInput.style.textDecoration = 'none';
+        editBtn.textContent = '저장';
+      } else if (editBtn.textContent === '저장') {
+        editInput.setAttribute('readonly', 'readonly');
+        editInput.blur();
+        editBtn.textContent = '수정';
+      }
+    });
+  });
+
   // todo 갯수 업데이트
   const updateToDoCount = () => {
     const todoTotalCount = $('.main__todo').querySelectorAll('li').length;
@@ -12,18 +34,18 @@ function App() {
   const addTodo = () => {
     const todo = $('.main__input-text').value;
     const todoTemplate = (todo) => {
+      let isEditing;
+      console.log(isEditing);
       return `
         <li class="main__todo-list" id="todo-id">
           <div class="main__todo-list--title-container">
             <span class="main__todo-list--date">12-15</span>
-            <span class="main__todo-list--title"
-              >${todo}</span
-            >
+            <input readonly class="main__todo-list--title todo-title" value="${todo}"/>
           </div>
           <div class="main__todo-list--btn-container">
-            <button class="main__todo-list--done-btn btn">완료</button>
-            <button class="main__todo-list--edit-btn btn">수정</button>
-            <button class="main__todo-list--delete-btn btn">삭제</button>
+            <button class="main__todo-list--done-btn btn done-btn">완료</button>
+            <button class="main__todo-list--edit-btn btn edit-btn">수정</button>
+            <button class="main__todo-list--delete-btn btn delete-btn">삭제</button>
           </div>
         </li>`;
     };
@@ -37,22 +59,29 @@ function App() {
     e.preventDefault();
   });
 
-  // $('#todo-add-btn').addEventListener('click', () => {
-  //   addTodo();
-  // });
-
-  $('.main__input-text').addEventListener('keypress', (e) => {
-    if (e.key !== 'Enter') {
-      return;
-    }
+  $('#todo-add-btn').addEventListener('click', () => {
     if ($('.main__input-text').value === '') {
       alert('할 일을 입력해주세요.');
       return;
     }
-    if (e.key === 'Enter') {
-      addTodo();
-    }
+    addTodo();
+    console.log('btn');
   });
+
+  /** 엔터키 입력시 enter */
+  // $('.main__input-text').addEventListener('keypress', (e) => {
+  //   if (e.key !== 'Enter') {
+  //     return;
+  //   }
+  // if ($('.main__input-text').value === '') {
+  //   alert('할 일을 입력해주세요.');
+  //   return;
+  // }
+  //   if (e.key === 'Enter') {
+  //     addTodo();
+  //     console.log('enter');
+  //   }
+  // });
 }
 
 const app = new App();
