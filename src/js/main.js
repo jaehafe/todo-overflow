dd9607028999fcc00453c470605a4ee52384e282
Jaeha Lee <wogkdkrm112@gmail.com>
2023-01-19 17:46:38 +0900

    ✒️Feat: todo list 생성 함수(createTask) 추가

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,62 @@
 import '../scss/style.scss';
 
 const $ = (selector) => document.querySelector(selector);
+
+let tasks = [];
+
+$('#todo-add-btn').addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const inputValue = $('.main__input-text').value;
+
+  if (inputValue.trim() === '') {
+    alert('할 일을 입력하세요');
+    return;
+  }
+
+  const task = {
+    id: new Date().getTime(),
+    name: inputValue,
+    isCompleted: false,
+  };
+
+  tasks.push(task);
+
+  createTask(task);
+  $('.main__input-text').focus();
+});
+let today = new Date();
+let month = today.getMonth() + 1;
+let date = today.getDate();
+
+const createTask = (task) => {
+  if (task.isCompleted) {
+    $('.main__todo-list').classList.add('complete');
+  }
+  const template = `
+    <li class="main__todo-list" data-todo-id="${task.id}">
+      <div class="main__todo-list--title-container">
+        <span class="main__todo-list--date">${month}/${date}</span>
+        <span
+          ${!task.isCompleted ? 'contenteditable' : ''}
+          class="main__todo-list--title todo-title"
+          >${task.name}</span
+        >
+      </div>
+      <div class="main__todo-list--btn-container">
+        <button class="main__todo-list--done-btn btn done-btn">
+          완료
+        </button>
+        <button class="main__todo-list--edit-btn btn edit-btn">
+          수정
+        </button>
+        <button class="main__todo-list--delete-btn btn delete-btn">
+          삭제
+        </button>
+      </div>
+    </li>
+    `;
+
+  $('.main__todo').innerHTML += template;
+  $('.main__input-text').value = '';
+};
